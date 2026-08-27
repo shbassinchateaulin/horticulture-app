fe51cb707bb76114ba42f77147a7a0ecf23e5381
shbassinchateaulin <shbassinchateaulin@gmail.com>
2026-08-27 13:23:48 +0200

    Refresh cache after separating app icon and logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='horticulture-admin-v14';
+const CACHE='horticulture-admin-v15';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-admin-transparent.png','./app-icon-192-v3.png','./app-icon-512-v3.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
