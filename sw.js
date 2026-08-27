1a5a7a3bdd91567fee0d68033cd0edbd3ca675b3
shbassinchateaulin <shbassinchateaulin@gmail.com>
2026-08-27 14:34:10 +0200

    Refresh PWA cache for navigation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='horticulture-admin-v17';
+const CACHE='horticulture-admin-v19';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-admin-transparent.png','./app-icon-botanical-v4.png','./superadmin-v2.css','./superadmin-v2.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
