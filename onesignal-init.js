648e8642c029b54e1683b7fdc4d77e55c7d0ab0b
shbassinchateaulin <shbassinchateaulin@gmail.com>
2026-08-29 23:53:31 +0200

    Fix OneSignal worker path for GitHub project pages

--- onesignal-init.js
+++ onesignal-init.js
@@ -15,7 +15,7 @@
     await OneSignal.init({
       appId: "64cb7fb2-efb3-4008-9233-5a305d0f31a2",
       safari_web_id: "web.onesignal.auto.185a3882-a3fa-4e4c-9216-d752294e35fc",
-      serviceWorkerPath: "/horticulture-app/push/onesignal/OneSignalSDKWorker.js",
+      serviceWorkerPath: "horticulture-app/push/onesignal/OneSignalSDKWorker.js",
       serviceWorkerParam: { scope: "/horticulture-app/push/onesignal/" },
       notifyButton: { enable: false }
     });
